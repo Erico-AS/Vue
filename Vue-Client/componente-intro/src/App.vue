8636689fd4b2601f150ec0272d9ef6623cc63042
Érico Augusto <67467324+Erico-AS@users.noreply.github.com>
2024-04-15 13:19:09 -0300

    Add files via upload

---
+++ Vue-Client/componente-intro/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Projeto Vue - task 01"/>
+
+    <h2>{{ contador }}</h2>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  data() {
+    return {
+      HelloWorld,
+      contador: 0
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
